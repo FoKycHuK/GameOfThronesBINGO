a186b92e1728be75fbaaabf817f44f3652efe4a9
fokychuk_book <fokychuk47@ya.ru>
2016-04-30 12:51:51 +0500

    WeekProvider

---
+++ GOTB.WebUI/Infrastructure/WeekProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GoTB.WebUI.Infrastructure
+{
+    public class WeekProvider : IWeekProvider
+    {
+        public int GetCurrentWeek()
+        {
+            var startOfAllTime = DateTime.Parse("28/04/2016");
+            var passedTime = DateTime.Now - startOfAllTime;
+            return passedTime.Days / 7;
+        }
+    }
+}
